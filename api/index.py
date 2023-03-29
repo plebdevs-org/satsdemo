7ac263d1607b67f2c0619556549c1efac73dc3bf
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2023-03-29 15:49:31 -0700

    add placeholder for coingecko

--- api/index.py
+++ api/index.py
@@ -1,17 +1,22 @@
 from flask import Flask
 from . kraken_data import getTickerInfo
+# add . for finding path when using vercel dev
 
 app = Flask(__name__)
 
+ticker_symbol = 'XBTUSD'
+long_symbol = 'XXBTZUSD'
+
 def last_close_price():
-    ticker = getTickerInfo('XBTUSD')
-    last_trade_closed = ticker['XXBTZUSD']['c']
+    ticker = getTickerInfo(ticker_symbol)
+    last_trade_closed = ticker[long_symbol]['c']
     content = "Last Trade Closed Price: " + str(last_trade_closed[0])
     return content
 
+
 def last_ask_price():
-    ticker = getTickerInfo('XBTUSD')
-    last_ask = ticker['XXBTZUSD']['a']
+    ticker = getTickerInfo(ticker_symbol)
+    last_ask = ticker[long_symbol]['a']
     content = "Last Ask Price: " + str(last_ask[0])
     return content
 
