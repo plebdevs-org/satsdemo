07145f72e87aad1e1483c0a894493849868e4364
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2023-03-29 16:01:28 -0700

    add notes

--- api/coingecko_data.py
+++ api/coingecko_data.py
@@ -1,1 +1,6 @@
 # https://www.coingecko.com/en/api/documentation
+
+# TODO 
+# make a function to check the API server status
+# make functions to get BTC/USD price but also 4 other major currencies
+# also make a function to get BTC/USD price on a historical date
